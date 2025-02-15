f0ef4f9c39687e4c31208c58b0c7ac77e5dda037
Chris Osborn <fozztexx@fozztexx.com>
2025-02-14 18:49:14 -0800

    Able to open directory and get a listing.

---
+++ ncopy/fuji.h
@@ -0,0 +1,29 @@
+/* Contributed by fozztexx@fozztexx.com
+ */
+
+#ifndef _NET_H
+#define _NET_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <time.h>
+
+typedef int errcode;
+typedef struct {
+  const char *name;
+  off_t size;
+  time_t mtime;
+  unsigned char isdir:1;
+} FN_DIRENT;
+
+// FIXME - this should probably return a handle to point to the network device which was used?
+extern errcode fuji_open_url(const char *url);
+extern errcode fuji_close_url();
+extern size_t fuji_read(uint8_t *buf, size_t length);
+extern errcode fuji_opendir();
+extern errcode fuji_closedir();
+extern FN_DIRENT *fuji_readdir();
+extern errcode fuji_chdir();
+
+#endif /* _NET_H */
